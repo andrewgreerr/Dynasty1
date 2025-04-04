ae71378163248951c8a3a4b5e4e28daad0b6b855
andrewgreerr <60408358+andrewgreerr@users.noreply.github.com>
2025-04-03 23:09:43 -0400

    Comments main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 extracted_data ={}
 
+# Static dictionary to map HTML file to Team ID 
 users = {
     "donnelly.html" : '739542804027531264',
     "greer.html" : '1000545944431063040',
@@ -22,28 +23,28 @@ users = {
     "decker.html" : '1127783132725444608'
 }
 
+# *** Renewing a Dynasty league on sleeper technically creates a new league and the league ID must be changed for GET requests to be accurate ***
+
+# Gets rosters of league based on our league ID
 def fetch_rosters():
     url = 'https://api.sleeper.app/v1/league/1113520351100637184/rosters'
     response = requests.get(url)
-    #print(response)
-    # Handle possible errors
     if response.status_code == 200:
-        #print(response.json())
         return response.json()
     else:
         response.raise_for_status()
 
+# Gets user ID for each memeber of the league
 def fetch_users():
     url = 'https://api.sleeper.app/v1/league/1113520351100637184/users'
     response = requests.get(url)
     print(response)
-    # Handle possible errors
     if response.status_code == 200:
-        #print(response.json())
         return response.json()
     else:
         response.raise_for_status()
 
+# '/' is the home HTML page, anytime this page is reached it will call the fetch_rosters function to pull newest rosters
 @app.route('/')
 def serve_html():
     try:
@@ -57,17 +58,11 @@ def serve_html():
     except requests.RequestException as e:
         return f"An error occurred: {e}", 500
 
+# Static page to show all values of players 
 @app.route('/rawdata.html')
 def raw():
     return render_template("rawdata.html")
 
-#@app.route('/brian.html')
-#def brian_player():
-#    print("Hit")
-#    for entry in extracted_data:
-#        if entry['owner_id'] == "1124689612208713728":
-#            print(entry["players"])
-#            return render_template('brian.html')
 
 @app.route('/run-function', methods=['GET'])
 def run_function():
@@ -75,20 +70,22 @@ def run_function():
     fetch_users()
     return jsonify({"message": "Python function executed successfully."})
 
+
+# Dynamic function, takes in the url filename being reach [User's name]
 @app.route('/<filename>')
 def serve_other_files(filename):
     user_id = users[filename]
     split = filename.split('.')
     print(split[0], " : ", extracted_data[user_id])
-    with open("templates/" + filename, 'r', encoding='utf-8') as file:
+    with open("templates/" + filename, 'r', encoding='utf-8') as file:    #Extracts name of user from the url link and reads the HTML that is currently saved
         soup = BeautifulSoup(file, 'html.parser')
-        tbody_element = soup.find(('tbody'))
+        tbody_element = soup.find(('tbody'))        # Finds part section of the HTML that contains the table with the information of the user's roster
 
         if tbody_element:
-            tbody_element.clear()
+            tbody_element.clear()        # Clears out all of the information that is in the table
 
-            conn = sqlite3.connect('players.db')
-            pvalue_conn = sqlite3.connect('PValue.db')
+            conn = sqlite3.connect('players.db')        # Connects to the player information database
+            pvalue_conn = sqlite3.connect('PValue.db')        # Connects to the database containing the each player's salary 
             cursor = conn.cursor()
             pvalue_cursor = pvalue_conn.cursor()
 
@@ -98,22 +95,22 @@ def serve_other_files(filename):
 
             rows_data = []
 
+            # Loop through all of the user's roster information
             for person in extracted_data[user_id]:
                 try:
-                    # Query to select data from the players table where player_id matches
+                    # for each player, retrieve the specified information from player information database
                     cursor.execute('SELECT first_name, last_name, age, position FROM players WHERE player_id = ?',
                                    (person,))
 
-                    # Fetch the result of the query
                     row = cursor.fetchone()
 
                     if row:
-                        full_name = f"{row[0]} {row[1]}"
+                        full_name = f"{row[0]} {row[1]}"        # Combines first and last name to get full name
                         print(f"Querying PValue.db for: {full_name}")
-                        pvalue_cursor.execute('SELECT Value FROM player_value WHERE TRIM(Name) = ?', (full_name,))
+                        pvalue_cursor.execute('SELECT Value FROM player_value WHERE TRIM(Name) = ?', (full_name,))  # searches for player value using full name
                         pvalue_row = pvalue_cursor.fetchone()
 
-                        value = pvalue_row[0] if pvalue_row else 1
+                        value = pvalue_row[0] if pvalue_row else 1        # If the player doesn't have a value, they will automatically be given the value of 1
                         total_value += value
 
                         total_age += row[2]
@@ -132,10 +129,10 @@ def serve_other_files(filename):
                 except sqlite3.Error as e:
                     print(f"An error occurred: {e}")
 
-            # Sort the rows by value in descending order
+            # Sort the rows by value from high to low
             rows_data.sort(key=lambda x: x['value'], reverse=True)
 
-            # Append the sorted rows to the tbody element with updated roster number
+            # Add new entry in the table for each player on the user's roster
             for index, data in enumerate(rows_data, start=1):
                 tbody_element.append(BeautifulSoup("<tr>" +
                                                    "<td>" +
@@ -186,10 +183,6 @@ def serve_other_files(filename):
 
     return render_template(filename)
 
-
-
-def get_teams():
-    print("Python function is running...")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
